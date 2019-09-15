2452b094f9c5e806438c17c17be9718ea9514ba4
jiyinyiyong <jiyinyiyong@gmail.com>
2019-09-15 13:25:34 +0800

    fetched README files

--- src/app/dl.cljs
+++ src/app/dl.cljs
@@ -5,20 +5,38 @@
             [cljs.reader :refer [read-string]]
             [clojure.string :as string]
             [applied-science.js-interop :as j]
-            ("fs" :as fs))
+            ["fs" :as fs]
+            [clojure.core.async :refer [go go-loop <! >!]]
+            [chan-utils.core :refer [chan-once]])
   (:require-macros [clojure.core.strint :refer [<<]]))
+
+(defn chan-download-doc [project-name link]
+  (chan-once
+   got
+   (-> (axios/get link)
+       (.then
+        (fn [response]
+          (fs/writeFileSync (str "data/files/" project-name ".md") (j/get response :data))
+          (println "Wrote to" project-name)
+          (got true)))
+       (.catch (fn [error] (js/console.error "Failed at fetching:" link))))))
 
 (defn main! []
   (let [projects (read-string (inline "projects.edn"))
         flat-projects (mapcat :projects projects)
         repos (map :repo flat-projects)
-        project-names (map (fn [link] (string/replace link "https://github.com/" "")) repos)]
-    (doseq [project-name project-names]
-      (let [link (<< "https://raw.githubusercontent.com/~{project-name}/master/README.md")]
-        (-> (axios/get link)
-            (.then
-             (fn [response]
-               (fs/writeFileSync
-                (str "data/files/" project-name ".md")
-                (j/get response :data))
-               (println "Wrote to" project-name))))))))
+        project-names (->> repos
+                           (filter
+                            (fn [link]
+                              (not
+                               (or (string/includes? link "/ace")
+                                   (string/includes? link "/pygments-main")))))
+                           (map (fn [link] (string/replace link "https://github.com/" ""))))]
+    (println "There are " (count repos) "projects")
+    (go-loop
+     [xs (drop 60 project-names) c 1]
+     (let [project-name (first xs)
+           link (<< "https://raw.githubusercontent.com/~{project-name}/master/README.md")]
+       (<! (chan-download-doc project-name link))
+       (println "Finished" c "projects... More:" (pr-str (take 3 xs)))
+       (if (empty? (rest xs)) (println "All finished.") (recur (rest xs) (inc c)))))))
